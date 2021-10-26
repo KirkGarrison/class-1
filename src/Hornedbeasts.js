c415c7ed0ed9862b06aadfc7c0b7a506fd8a15cd
Kirk Garrison <Kirkgarrison24601@gmail.com>
2021-10-26 15:43:36 -0700

    Need to install bootstrap for next step. Steps 1-2 of lab completed

--- src/Hornedbeasts.js
+++ src/Hornedbeasts.js
@@ -1,12 +1,27 @@
 import { Component } from "react";
 
 class Hornedbeasts extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            imageclicked: 0
+        }
+    }
+
+    handleclick = () => {
+        this.setState({imageclicked: this.state.imageclicked + 1});
+    }
+
     render() {
         return (
             <>
                 <h2>{this.props.title}</h2>
-                <img src={this.props.image} alt={this.props.alt} title={this.props.imgtitle} />
+                <img src={this.props.image} alt={this.props.alt} title={this.props.imgtitle} onClick={this.handleclick}/>
                 <p>{this.props.description}</p>
+                <p>{this.props.keyword}</p>
+                <p>{this.props.horns}</p>
+                <p>&hearts;: {this.state.imageclicked}</p>
             </>
         )
     }

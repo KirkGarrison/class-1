e9276132e165b018ab3e56115616bd474ad07089
Kirk Garrison <Kirkgarrison24601@gmail.com>
2021-10-26 17:01:59 -0700

    Added bootstrap container

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React, { Component } from 'react'
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
+import { Container, Row, Col } from 'react-bootstrap';
+
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <Header />
-        <Main />
-        <Footer />
+      <div class="container">
+        <Container>
+          <Row>
+            <Col><Header /></Col>
+            <Col><Main /></Col>
+            <Col><Footer /></Col>
+          </Row>
+        </Container>
       </div>
     )
   }
